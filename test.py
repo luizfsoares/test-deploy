20b2dc0fbb34a400fd126c51b6db68c3fd5cf299
Luiz Felipe Soares <38364154+luizfsoares@users.noreply.github.com>
2021-07-26 19:59:14 -0300

    dale

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import requests
 
 API_URL = "https://api-inference.huggingface.co/models/Felipehonorato/storIA"
@@ -24,31 +23,4 @@ size = 50 # DEFINIDO PELO USUÁRIO DEFAULT 50
 input = check_token(input)
 output = query({"inputs": input,
                 "parameters": {"max_length": 50, 'repetition_penalty': float(1.2), 'num_beams':5,
-=======
-import requests
-
-API_URL = "https://api-inference.huggingface.co/models/Felipehonorato/storIA"
-headers = {"Authorization": "Bearer api_CwzaLVoNBMVQhviuBtnxxdVoXvQgjuTEmW"}
-
-def remove_token(text):
-  return " ".join(text.split()[1:])
-
-def check_token(input):
-  token = '<|startoftext|> '
-  if input.split()[0] != token:
-    return token + input
-  else:
-    return input
-
-def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.json()
-
-input = 'it was a dark night' # TEXTO DO USUÁRIO
-input_len = len(input.split())
-size = 50 # DEFINIDO PELO USUÁRIO DEFAULT 50
-input = check_token(input)
-output = query({"inputs": input,
-                "parameters": {"max_length": 50, 'repetition_penalty': float(1.2), 'num_beams':5,
->>>>>>> be35fd2ae843dd04c6267745f84e7374c3c3a4a3
                                'no_repeat_ngram_size':3, 'max_length':input_len + size}})
